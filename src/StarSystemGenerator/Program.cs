8fc58a2d8f315a7db9cf216639e16c8b215b23d8
Valerij Kozlov <valerij.kozlov@gmail.com>
2017-10-18 23:59:36 +0100

    Solution and blanc console app

---
+++ src/StarSystemGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SalaryFinanceHomework.StarSystemGenerator
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Generating planets from interspace void");
+
+            return 0;
+        }
+    }
+}
