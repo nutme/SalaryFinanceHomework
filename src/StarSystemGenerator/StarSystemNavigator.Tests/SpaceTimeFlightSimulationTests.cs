8eb347d6e7936be6221e7530f84eedd7eeef92c2
Valerij Kozlov <valerij.kozlov@gmail.com>
2017-10-19 11:57:22 +0100

    Tests for space time simulator

---
+++ src/StarSystemGenerator/StarSystemNavigator.Tests/SpaceTimeFlightSimulationTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using SalaryFinanceHomework.StarSystemCore;
+using SalaryFinanceHomework.StarSystemGenerator.Worker;
+using SalaryFinanceHomework.StarSystemNavigator.Worker;
+using System.Linq;
+
+namespace SalaryFinanceHomework.StarSystemNavigator.Tests
+{
+    [TestFixture]
+    public class SpaceTimeFlightSimulationTests
+    {
+        [Test]
+        public void CanCalculateSurfaceOfHabitablePlanetsInArray()
+        {
+            var numberOfSpaceObjects = 15000;
+            var spacePopulator = new SpacePopulator(new CryptoRandomnessProvider());
+            var spaceArray = spacePopulator.Run(numberOfSpaceObjects);
+
+            var simulator = new SpaceTimeFlightSimulation(new StarSystemFilter());
+
+            var planets = spaceArray.Where(o => o.SpaceObjectType == StarSystemCore.SpaceObjectType.Planet);
+            var visitedPlanets = planets.Take(200);
+
+            var testSumOfSurface = visitedPlanets.Select(p => ((Planet)p).Habitable ? ((Planet)p).SurfaceArea : 0).Sum();
+
+            var surface = simulator.CalculateHabitablePlanetsSurface(visitedPlanets);
+
+            Assert.That(surface, Is.EqualTo(testSumOfSurface));
+
+        }
+    }
+}
---
+++ src/StarSystemGenerator/StarSystemNavigator.Tests/StarSystemFilterTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SalaryFinanceHomework.StarSystemCore;
+using SalaryFinanceHomework.StarSystemGenerator.Worker;
+using SalaryFinanceHomework.StarSystemNavigator.Worker;
+using System.Linq;
+
+namespace SalaryFinanceHomework.StarSystemNavigator.Tests
+{
+    [TestFixture]
+    public class StarSystemFilterTests
+    {
+        [Test]
+        public void FilterOutMostersFromSpaceArray()
+        {
+            var numberOfSpaceObjects = 15000;
+            var spacePopulator = new SpacePopulator(new CryptoRandomnessProvider());
+            var spaceArray = spacePopulator.Run(numberOfSpaceObjects);
+
+            var filter = new StarSystemFilter();
+            var planets = filter.FindTargetsForColonisation(spaceArray);
+
+            Assert.That(planets.Any(p => p.SpaceObjectType == SpaceObjectType.Monster), Is.False);
+        }
+    }
+}
