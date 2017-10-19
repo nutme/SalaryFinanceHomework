f762b02401da4a7c056751e6b8664dfacd2efccd
Valerij Kozlov <valerij.kozlov@gmail.com>
2017-10-19 03:47:17 +0100

    Wired up methods in console app to generate space

--- src/StarSystemGenerator/Program.cs
+++ src/StarSystemGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SalaryFinanceHomework.StarSystemCore;
+using SalaryFinanceHomework.StarSystemFileSystem;
 using SalaryFinanceHomework.StarSystemGenerator.Worker;
 using System;
 
@@ -8,9 +9,20 @@ namespace SalaryFinanceHomework.StarSystemGenerator
     {
         static int Main(string[] args)
         {
+            // creating instance of random generator & space object generator
             Console.WriteLine("Generating planets from interspace void");
+            var randomnessProvider = new CryptoRandomnessProvider();
+            var spacePopulator = new SpacePopulator(randomnessProvider);
 
+            // run accuall space generation
+            var randomSpace = spacePopulator.Run(SpaceRules.SpaceSystemCapacity);
 
+            // write results to the file
+            Console.WriteLine("Saving space to file store");
+            var fileStore = new SpaceStorage();
+            var fileName = fileStore.SaveSpace(randomSpace);
+
+            Console.WriteLine($"Saved space to file ${fileName}");
 
             return 0;
         }
