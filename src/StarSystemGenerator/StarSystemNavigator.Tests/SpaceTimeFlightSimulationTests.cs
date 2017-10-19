96cfd89b0693a23ccbbfcff2c4be3429473cee2b
Valerij Kozlov <valerij.kozlov@gmail.com>
2017-10-19 12:02:11 +0100

    Test for visited planets simulation

--- src/StarSystemGenerator/StarSystemNavigator.Tests/SpaceTimeFlightSimulationTests.cs
+++ src/StarSystemGenerator/StarSystemNavigator.Tests/SpaceTimeFlightSimulationTests.cs
@@ -6,9 +6,24 @@ using System.Linq;
 
 namespace SalaryFinanceHomework.StarSystemNavigator.Tests
 {
+    // TODO: refactor those test to use some sort of helper to create space objects
+    // or load one test scenario from resource
     [TestFixture]
     public class SpaceTimeFlightSimulationTests
     {
+        [Test]
+        public void SimulationReturnShipsVisitedPlanets()
+        {
+            var numberOfSpaceObjects = 15000;
+            var spacePopulator = new SpacePopulator(new CryptoRandomnessProvider());
+            var spaceArray = spacePopulator.Run(numberOfSpaceObjects);
+
+            var simulator = new SpaceTimeFlightSimulation(new StarSystemFilter());
+            var visitedPlanets = simulator.Run(spaceArray);
+
+            Assert.That(visitedPlanets, Is.Not.Empty);
+        }
+
         [Test]
         public void CanCalculateSurfaceOfHabitablePlanetsInArray()
         {
@@ -18,7 +33,7 @@ namespace SalaryFinanceHomework.StarSystemNavigator.Tests
 
             var simulator = new SpaceTimeFlightSimulation(new StarSystemFilter());
 
-            var planets = spaceArray.Where(o => o.SpaceObjectType == StarSystemCore.SpaceObjectType.Planet);
+            var planets = spaceArray.Where(o => o.SpaceObjectType == SpaceObjectType.Planet);
             var visitedPlanets = planets.Take(200);
 
             var testSumOfSurface = visitedPlanets.Select(p => ((Planet)p).Habitable ? ((Planet)p).SurfaceArea : 0).Sum();
